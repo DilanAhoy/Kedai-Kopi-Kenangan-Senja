628a56822d6d68a9ea52b6523fb081008e1ffa20
dilanahoy <dilanahoyy@gmail.com>
2025-06-27 03:08:48 +0700

    update website

--- src/app.js
+++ src/app.js
@@ -27,6 +27,8 @@ document.addEventListener("alpine:init", () => {
         price: 40000,
       },
     ],
+    showModal: false, // Tambahkan ini untuk mengatur status modal
+    selectedItem: null, // Untuk menyimpan item yang dipilih
   }));
 
   Alpine.store("cart", {
@@ -87,6 +89,49 @@ document.addEventListener("alpine:init", () => {
     },
   });
 });
+
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disable = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disable");
+      checkoutButton.classList.add("disable");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disable = false;
+  checkoutButton.classList.remove("disable");
+});
+
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6282133766676?text=" + encodeURIComponent(message));
+});
+
+// Format Pesan WhatsApp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+  Data Pesanan
+  ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+  )}
+TOTAL: ${rupiah(obj.total)}
+Terima Kasih.`;
+};
 
 // konversi ke Rupiah
 const rupiah = (number) => {
